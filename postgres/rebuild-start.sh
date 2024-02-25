74fc9d2b7965ed0f4727248fbdc4c094c01e2053
kwojcik <Kamil.A.Wojcik@protonmail.com>
2024-02-26 00:04:52 +0100

    docker&frotnend improvements and fixes

--- postgres/rebuild-start.sh
+++ postgres/rebuild-start.sh
@@ -1,6 +1,6 @@
-sudo docker container kill odwsi-db;
-sudo docker container rm odwsi-db;
-sudo docker image rm odwsi-db;
+sudo podman container kill odwsi-db;
+sudo podman container rm odwsi-db;
+sudo podman image rm odwsi-db;
 
-sudo docker build ./ --tag odwsi-db;
-sudo docker run -p 5432:5432 --name odwsi-db odwsi-db
+sudo podman build ./ --tag odwsi-db;
+sudo podman run -p 5432:5432 --name odwsi-db odwsi-db
